7a61453055e2c5295985c275006129b383356dac
RJlidi <rayanjlidi12@gmail.com>
2023-09-21 15:50:33 +0300

    Fixed image resizing

--- js/style.js
+++ js/style.js
@@ -12,18 +12,23 @@ const resizeCards = cards => {
         card.style.paddingBottom = `${parseInt(styles.paddingBottom) + (tallestHeight - parseInt(styles.height))}px`;
     });
 }
+
+const resizeImg = (img, ref) => {
+    img && ref && (img.style.height = `calc(100% - ${getComputedStyle(ref).height} - 3rem)`);
+}
+
 if (refImg)
     refImg.onload = () => {
-        (img.style.height = `calc(100% - ${getComputedStyle(refImg).height} - 3rem)`);
-    }
+        resizeImg(img, refImg);
+    };
 
 window.addEventListener("load", () => {
-    refImg && (img.style.height = `calc(100% - ${getComputedStyle(refImg).height} - 3rem)`);
+    resizeImg(img, refImg);
     cards && cards.length && resizeCards(cards);
 
 });
 
-
+window.addEventListener("resize", () => resizeImg(img, refImg));
 
 langSwitcher.addEventListener("click", () => {
     cards && cards.length && resizeCards(cards);
